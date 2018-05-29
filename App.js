f07a6c24730ca97ee2fd016bccd6293908e48ebe
thienth32 <thienth32@gmail.com>
2018-05-29 15:05:19 +0700

    update

--- App.js
+++ App.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, TextInput, Image,
           View, TouchableOpacity } from 'react-native';
-import Header from './Header';
+
+
+const resizeMode = 'center';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: 'PT12352-mob',
-      age: 2.5
-    }
+    
   }
 
   render() {
+    
     return (
-      <View style={styles.container}>
-        <Image 
-          source={require('./img/con-cho.jpg')}
-          style={{width: '100%', height: 300}}/>
-        <Header tencuaban="something v2"/>
-        <TextInput 
-          onChangeText={(val) => this.setState({name: val})}
-          style={{fontSize: 20, width: '100%'}} placeholder="Nhap Name vao trong nay"/>
-        <Text style={{fontSize: 20}}>Tên: {this.state.name}</Text>
-        <Text style={{fontSize: 20}}>Tuổi: {this.state.age}</Text>
-      </View>
+      <Image source={require('./img/logo.jpg')} style={styles.bgImage}>
+
+      </Image>
     );
   }
 }
@@ -35,4 +27,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bgImage: {
+    backgroundColor: '#ccc',
+    flex: 1,
+    resizeMode,
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+  }
+  
 });
